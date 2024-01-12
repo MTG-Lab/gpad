a338412d0b345486c4799e59c3b430fd2ad7e99d
Tahsin Hassan Rahit <tahsin.rahit@gmail.com>
2024-01-12 03:01:38 -0700

    🚀 Initialize MongoDB with necessary database and collections

---
+++ init-db/init-mongo.js
@@ -0,0 +1,28 @@
+// init-mongo.js
+db = db.getSiblingDB('gene_discovery');  // Create new database or connect to it if it already exists
+
+// Create collections
+db.createCollection("omim_entry_AGG");  // For saving OMIM entries
+db.createCollection("association_information_test_2_AGG");  // For saving association information after curation
+
+// Create indexes for collections
+db.omim_entry_AGG.createIndex([
+    { v: 2, key: { _id: 1 }, name: '_id_' },
+    {
+      v: 2,
+      key: { _fts: 'text', _ftsx: 1 },
+      name: 'text',
+      background: false,
+      weights: {
+        'geneMap.approvedGeneSymbols': 1,
+        'geneMap.ensemblIDs': 1,
+        'geneMap.geneName': 1,
+        'geneMap.geneSymbols': 1
+      },
+      default_language: 'english',
+      language_override: 'language',
+      textIndexVersion: 3
+    },
+    { v: 2, key: { _cls: 1 }, name: '_cls_1', background: false }
+  ]); 
+
